896b4196f2b5c90ae4286bf5835aababa5b59048
Nwaigba66 <glorianwaigba@gmail.com>
2024-01-01 22:24:20 +0000

    Added a Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""This module define a Server class and index_range function
+"""
+import csv
+import math
+from typing import List, Dict
+
+
+def index_range(page, page_size):
+    """Get the corresponding page range for a given page size and number
+    """
+    start = (page - 1) * page_size
+    return (start, start + page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get pages from dataset
+        """
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert (page > 0 and page_size > 0)
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Define hypermedia pagination
+        """
+        data = self.get_page(page, page_size)
+        prev_page = None if page == 1 else page - 1
+        next_start, next_end = index_range(page+1, page_size)
+        next_page = None if len(self.dataset()) - 1 < next_start else page + 1
+        return dict(
+                page_size=0 if not data else page_size,
+                page=page,
+                data=data,
+                next_page=next_page,
+                prev_page=prev_page,
+                total_pages=math.ceil(len(self.dataset())/page_size))
